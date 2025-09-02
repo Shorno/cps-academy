f14af6ac1fbe97a6488d78d147c6e9e7ae5b2646
Shorno <fb.shorno@gmail.com>
2025-09-02 14:28:29 +0600

    folders check

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -3,7 +3,8 @@ import {HeroSection} from "@/components/landing/hero-section";
 export default async function Home() {
     return (
         <div className={""}>
-           <HeroSection/>
+            test
+            <HeroSection/>
         </div>
     );
 }
