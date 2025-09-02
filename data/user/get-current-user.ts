160f00f99dc1c9bb977c220c261f33943ef11a5a
Shorno <fb.shorno@gmail.com>
2025-09-02 21:08:03 +0600

    update endpoint

--- data/user/get-current-user.ts
+++ data/user/get-current-user.ts
@@ -2,11 +2,12 @@
 
 import {getAuthCookie} from "@/utils/getAuthCookie";
 import {User} from "@/actions/auth/login";
+import {meEndpoint} from "@/utils/endpoints";
 
 export async function getCurrentUser() : Promise<User> {
     const token = await getAuthCookie();
 
-    const response = await fetch('http://localhost:1337/api/users/me', {
+    const response = await fetch(meEndpoint, {
         headers: {
             'Authorization': `Bearer ${token}`,
             'Content-Type': 'application/json'
